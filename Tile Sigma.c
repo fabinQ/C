9b9c0728474dc9d412617cdaff8e1e16ba6d6c3c
fabinQ <maciejfabinski@gmail.com>
2023-10-18 23:01:46 +0200

    update

--- Tile Sigma.c
+++ Tile Sigma.c
@@ -1,40 +1,81 @@
 #include<stdio.h> //obsluguje operacje wyjscia wejscia
 #include<stdlib.h> // dostarcza inne podstawowe funckcje
 
-int i =0;
+int i,j,k =0;
 int d, gd;
 
 int main()
 {
     printf("Podaj liczbe calkowita: ");
     scanf(" %d", &d);
-    printf("%d", d);
+    printf("d: %d", d);
 
     gd = d/2;
-    printf("\n%d\n",gd);
+    printf("\ngd: %d\n",gd);
 
-    if (gd<1)
+    if (gd<2)
     {
         printf("Nie mozna utworzyc sigmy");
     }
     else{
-        for (i=0; i<gd; i++)
+        for (i=0; i<(d+2*gd); i++)  // Petla leci n razy plus gora i dół równy połowie d czyli 2gd
         {
-            for (int j=0 ; j<d; j++)
-            {
-                if (i==0)
+            if (i==0 || i==(d+2*gd)-1)  // górny i dolny pasek
                 {
-                    printf("#");
-                }
-                else{
-                    printf("#");
-                    for(int k = 0; k<d-2;k++)
+                    for (j=0; j<d; j++)
                     {
-                        printf("*");
+                        printf("#");
                     }
-                    printf("#\n");
                 }
+            else
+            {
+                if(i<gd || i>(d+gd-1))
+                {
+                    for (j=0 ; j<d; j++)
+                    {                     
+                        if (j>0 && j<d-1)
+                        {
+                            printf("*");
+                        }
+                        else
+                        {
+                            printf("#");
+                        }
+                    }
+                }
+                else
+                {
+                    
+                    if (k<gd)
+                    {
+                        for (j=0 ; j<d; j++)
+                        {                     
+                            if (j==k)
+                            {
+                                printf("%d \\",k);
+                                k++;
+                            }
+                            else
+                            {
+                                printf("#");
+                            }
+                        }
+                        
+                    }
+                    else if (k == d/2 && k%2 == 0)
+                    {
+                        printf("%d \\#",k);
+                        k++;
+                    }
+                    else
+                    {
+                        printf("%d /",k);
+                        k++;
+                    }
+                }
+                
             }
+            
             printf("\n");
             
             
